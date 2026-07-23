27ce1a68797c3bd1bd1912dcfb6fbf3b59057c29
VincenzoLivigni <enzolivigni13@gmail.com>
2026-07-23 17:01:08 +0200

    Aggiunta  sistema di autenticazione utenti con JWT

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,11 +1,12 @@
 const db = require("../config/db")
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 exports.register = (req, res) => {
     const { email, password } = req.body
 
     if (!email || !password) {
-        return res.status(400).json({ message: "Email e password devono essere inseriti" })
+        return res.status(400).json({ message: "Email e password non inserite" })
     }
 
     const hashedPassword = bcrypt.hashSync(password, 10)
@@ -15,10 +16,50 @@ exports.register = (req, res) => {
         [email, hashedPassword],
         (error, result) => {
             if (error) {
-                return res.status(500).json({ message: "Errore durante la connessione al server" })
+                return res.status(500).json({ message: "Errore durante la registrazione" })
             }
 
-            res.status(201).json({ message: "Utente Registrato con successo", id: result.insertId })
+            res.status(201).json({ message: "Utente registrato con successo", id: result.insertId })
+        }
+    )
+}
+
+
+exports.login = (req, res) => {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        return res.status(400).json({ message: "Email e password non inserite" })
+    }
+
+    db.query(
+        "SELECT * FROM users WHERE email = ?",
+        [email],
+        (error, result) => {
+            if (error) {
+                return res.status(500).json({ message: "Errore durante il login" })
+            }
+
+            if (result.length === 0) {
+                return res.status(401).json({ message: "Email inesistente" })
+            }
+
+            const user = result[0]
+
+            const matchedPassword = bcrypt.compareSync(password, user.password)
+            if (!matchedPassword) {
+                return res.status(401).json({ message: "Email o password non valide" })
+            }
+
+            const token = jwt.sign({
+                id: user.id,
+                email: user.email
+            },
+                process.env.JWT_SECRET,
+                { expiresIn: "9h" }
+            )
+
+            res.status(200).json({ message: "Login effettuato con successo", token })
         }
     )
 }
