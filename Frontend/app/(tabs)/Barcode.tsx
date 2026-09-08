119768d5753ee2f830abe711b00e133400b51d51
gilli-jonathan <gilli.jonathan97@gmail.com>
2026-09-08 13:15:21 +0200

    aggiunta la logica e implementato la fotocamera per la lettura dei barcode, nell'omonima pagina

--- Frontend/app/(tabs)/Barcode.tsx
+++ Frontend/app/(tabs)/Barcode.tsx
@@ -1,59 +1,134 @@
-import { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { CameraView, useCameraPermissions } from 'expo-camera';
-import { addProductToPantry } from '../../services/api';
+import {
+  Pressable,
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TextInput,
+  Alert,
+  ScrollView,
+} from "react-native";
+import { CameraView, useCameraPermissions } from "expo-camera";
+import { usePantry } from "../../contexts/pantryContext";
+import { useState } from "react";
+import { addProductToPantry } from "../../services/api";
 
-export default function Barcode() {
-    const [permission, requestPermission] = useCameraPermissions();
-    const [scanned, setScanned] = useState(false);
+export default function Formcode() {
+  //stati per usare la camera con i relativi permessi
+  const [scanned, setScanned] = useState(false);
+  const [permission, requestPermission] = useCameraPermissions();
 
-    if (!permission) return <View />
-    if (!permission.granted) {
-        return (
-            <View style={styles.container}>
-                <Text>Serve il permesso per usare la fotocamera</Text>
-                <Button onPress={requestPermission} title="Concedi permesso" />
-            </View>
-        );
-    }
+  //stati con i dettagli del prodotto
+  const [pantryId, setPantryId] = useState<number>(1);
+  const [barcodeScan, setBarcodeScan] = useState<string>("");
+  const [quantity, setQuantity] = useState("1");
+  const [expiring, setExpiring] = useState<string>(
+    new Date().toISOString().split("T")[0],
+  );
 
-    const currentPantryId = 25
+  const { pantries } = usePantry();
 
-    const handleBarCodeScanned = async ({ data: barcode }: { data: string }) => {
-        setScanned(true);
-        console.log(`Codice a barre: ${barcode}`)
-        try {
-            const qty = 1
-            const expirationDate = new Date().toISOString().split('T')[0]
-
-            const data = await addProductToPantry(currentPantryId, barcode, qty, expirationDate)
-
-        }
-        catch (err) {
-            console.log("Errore nell'aggiunta del prodotto:", err)
-        }
-    };
-
+  if (!permission) return <View />;
+  if (!permission.granted) {
     return (
-        <View style={styles.container}>
-            <CameraView
-                style={{ width: "100%", height: "100%" }}
-                facing="back"
-                barcodeScannerSettings={{
-                    barcodeTypes: ["ean13", "ean8"],
-                }}
-                onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
-            />
-            {scanned && (
-                <Button title={'Scansiona un altro prodotto'} onPress={() => setScanned(false)} />
-            )}
-        </View>
+      <View>
+        <Text>Serve il permesso per usare la fotocamera</Text>
+        <Button onPress={requestPermission} title="Concedi permesso" />
+      </View>
     );
+  }
+
+  const handleBarCodeScanned = ({ data: barcode }: { data: string }) => {
+    if (scanned) return;
+    setScanned(true);
+    setBarcodeScan(barcode);
+  };
+
+  const createProduct = async () => {
+    try {
+      await addProductToPantry(
+        pantryId,
+        barcodeScan,
+        parseInt(quantity) || 1,
+        expiring,
+      );
+
+      Alert.alert("prodotto aggiunto con successo");
+      setBarcodeScan("");
+      setScanned(false);
+    } catch (err) {
+      console.log("Errore nell'aggiunta del prodotto:", err);
+      Alert.alert("impossibile aggiungere il prodotto");
+    }
+  };
+
+  return (
+    <>
+      {/* SEZIONE PER LO SCAN DEL BARCODE */}
+      <View style={styles.container}>
+        {!barcodeScan ? (
+          <CameraView
+            style={{ width: "100%", height: "100%" }}
+            facing="back"
+            barcodeScannerSettings={{
+              barcodeTypes: ["ean13", "ean8"],
+            }}
+            onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
+          />
+        ) : (
+          <ScrollView>
+            {/* SEZIONE PER PASSARE PANTRY ID */}
+            {pantries.map((p) => (
+              <Pressable key={p.id} onPress={() => setPantryId(p.id)}>
+                <Text>{p.name}</Text>
+              </Pressable>
+            ))}
+
+            {/* SEZIONE PER LA QUANTITA' */}
+            <View>
+              <Text style={styles.label}>Quantità:</Text>
+              <TextInput
+                placeholder="inserisci la quantitÃ "
+                keyboardType="numeric"
+                value={quantity}
+                onChangeText={setQuantity}
+              />
+            </View>
+
+            {/* SEZIONE PER LA SCADENZA' */}
+            <View>
+              <Text style={styles.label}>Data di scadenza:</Text>
+              <TextInput
+                placeholder="inserisci la scadenza"
+                value={expiring}
+                onChangeText={setExpiring}
+              />
+            </View>
+
+            {/* CONFERMA CREAZIONE PRODOTTO' */}
+            <View>
+              <Button title="crea prodotto" onPress={createProduct} />
+              <Button
+                title="annula"
+                onPress={() => {
+                  setBarcodeScan("");
+                  setScanned(false);
+                }}
+              />
+            </View>
+          </ScrollView>
+        )}
+      </View>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  label: {
+    color: "red",
+  },
 });
